fa4dfb506d857a4cdcca2577b38c7f1642e77ff0
fotisz <fotisz@users.noreply.github.com>
2017-04-18 15:47:20 +0100

    Create build.js

---
+++ build.js
@@ -0,0 +1,9 @@
+({
+    baseUrl: '',
+    paths: {
+		'd3': 'd3.v3.min',
+		'd3_zoomable_circle_packing': 'd3_zoomable_circle_packing'
+    },
+    name: "d3_zoomable_circle_packing",
+    out: "d3_zoomable_circle_packing.min.js"
+})
